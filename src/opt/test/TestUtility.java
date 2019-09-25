5e7e133e681a6fa9451d7784d80c176b82b1c140
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-09-24 22:44:01 -0500

    Added MIMIC and RHC utility test suites

--- src/opt/test/TestUtility.java
+++ src/opt/test/TestUtility.java
@@ -2,9 +2,12 @@ package opt.test;
 
 import opt.EvaluationFunction;
 import opt.HillClimbingProblem;
+import opt.RandomizedHillClimbing;
 import opt.SimulatedAnnealing;
 import opt.ga.GeneticAlgorithmProblem;
 import opt.ga.StandardGeneticAlgorithm;
+import opt.prob.MIMIC;
+import opt.prob.ProbabilisticOptimizationProblem;
 import shared.FixedIterationTrainer;
 
 import java.io.BufferedWriter;
@@ -63,6 +66,54 @@ public class TestUtility {
 
     }
 
+    public static void testRHCWithDifferentProblemSizes(
+            List<Integer> problemSizeList,
+            List<HillClimbingProblem> problemList,
+            List<EvaluationFunction> evalFunctionList,
+            String directoryName) {
+
+        StringBuilder algorithmStats = new StringBuilder();
+
+        for (int k = 0; k < problemSizeList.size(); k++) {
+            algorithmStats.append(problemSizeList.get(k));
+            algorithmStats.append("\n");
+            HillClimbingProblem hcp = problemList.get(k);
+            EvaluationFunction ef = evalFunctionList.get(k);
+
+            // average the problem results 5 times
+            // get max value, number of function evaluations, and time taken to run
+            double totalOptimalValues = 0.0;
+            int totalFunctionEvaluations = 0;
+            long totalTimeTakenToRun = 0;
+            int trials = 5;
+            for (int j = 0; j < trials; j++) {
+                RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);
+                FixedIterationTrainer fit = new FixedIterationTrainer(rhc, 200000);
+                fit.train();
+                System.out.println(ef.value(rhc.getOptimal()));
+
+                // long startTime = System.currentTimeMillis();
+                fit.train();
+                // long endTime = System.currentTimeMillis();
+                totalOptimalValues += ef.value(rhc.getOptimal());
+                totalFunctionEvaluations += rhc.getTotalValueEvaluationsToFindOptimalValue();
+                totalTimeTakenToRun += rhc.getTimeTakenToFindBestValue();
+            }
+            algorithmStats.append(totalOptimalValues / trials);
+            algorithmStats.append(" ");
+            algorithmStats.append(totalFunctionEvaluations / trials);
+            algorithmStats.append(" ");
+            algorithmStats.append(totalTimeTakenToRun / trials);
+            algorithmStats.append("\n");
+            System.out.println("Avg Optimal Value: " + totalOptimalValues / trials);
+            System.out.println("Avg Number of functional evaluations: " + totalFunctionEvaluations / trials);
+            System.out.println("Avg time taken to run: " + totalTimeTakenToRun / trials);
+
+            System.out.println(algorithmStats);
+            writeTestStatsToFile(algorithmStats, directoryName, "RHC_Stats.txt");
+        }
+    }
+
     public static void testSimulatedAnnealingWithDifferentProblemSizes(
             List<Integer> problemSizeList,
             List<HillClimbingProblem> problemList,
@@ -73,7 +124,7 @@ public class TestUtility {
 
         StringBuilder algorithmStats = new StringBuilder();
 
-        for (int k = 0; k < problemList.size(); k++) {
+        for (int k = 0; k < problemSizeList.size(); k++) {
             algorithmStats.append(problemSizeList.get(k));
             algorithmStats.append(" ");
             algorithmStats.append(tList.size());
@@ -100,12 +151,12 @@ public class TestUtility {
                 for (int j = 0; j < trials; j++) {
                     SimulatedAnnealing sa = new SimulatedAnnealing(t, cooling, hcp);
                     FixedIterationTrainer fit = new FixedIterationTrainer(sa, 200000);
-                    long startTime = System.currentTimeMillis();
+                    // long startTime = System.currentTimeMillis();
                     fit.train();
-                    long endTime = System.currentTimeMillis();
+                    // long endTime = System.currentTimeMillis();
                     totalOptimalValues += ef.value(sa.getOptimal());
                     totalFunctionEvaluations += sa.getTotalValueEvaluationsToFindOptimalValue();
-                    totalTimeTakenToRun += endTime - startTime;
+                    totalTimeTakenToRun += sa.getTimeTakenToFindBestValue();
                 }
                 algorithmStats.append(totalOptimalValues / trials);
                 algorithmStats.append(" ");
@@ -129,11 +180,12 @@ public class TestUtility {
             List<Integer> populationList,
             List<Integer> toMateList,
             List<Integer> mutationList,
+            String algorithmType,
             String directoryName) {
 
         StringBuilder algorithmStats = new StringBuilder();
 
-        for (int k = 0; k < problemList.size(); k++) {
+        for (int k = 0; k < problemSizeList.size(); k++) {
             algorithmStats.append(problemSizeList.get(k));
             algorithmStats.append(" ");
             algorithmStats.append(populationList.size());
@@ -165,12 +217,12 @@ public class TestUtility {
                     fit.train();
                     System.out.println(ef.value(ga.getOptimal()));
 
-                    long startTime = System.currentTimeMillis();
+                    // long startTime = System.currentTimeMillis();
                     fit.train();
-                    long endTime = System.currentTimeMillis();
+                    // long endTime = System.currentTimeMillis();
                     totalOptimalValues += ef.value(ga.getOptimal());
                     totalFunctionEvaluations += ga.getTotalValueEvaluationsToFindOptimalValue();
-                    totalTimeTakenToRun += endTime - startTime;
+                    totalTimeTakenToRun += ga.getTimeTakenToFindBestValue();
                 }
                 algorithmStats.append(totalOptimalValues / trials);
                 algorithmStats.append(" ");
@@ -183,7 +235,68 @@ public class TestUtility {
                 System.out.println("Avg time taken to run: " + totalTimeTakenToRun / trials);
             }
             System.out.println(algorithmStats);
-            writeTestStatsToFile(algorithmStats, directoryName, "GA_Stats.txt");
+            writeTestStatsToFile(algorithmStats, directoryName, "GA_Stats_" + algorithmType +".txt");
+        }
+    }
+
+    public static void testMIMICWithDifferentProblemSizes(
+            List<Integer> problemSizeList,
+            List<ProbabilisticOptimizationProblem> problemList,
+            List<EvaluationFunction> evalFunctionList,
+            List<Integer> samplesList,
+            List<Integer> toKeepList,
+            String directoryName) {
+
+        StringBuilder algorithmStats = new StringBuilder();
+
+        for (int k = 0; k < problemSizeList.size(); k++) {
+            algorithmStats.append(problemSizeList.get(k));
+            algorithmStats.append("\n");
+            ProbabilisticOptimizationProblem pop = problemList.get(k);
+            EvaluationFunction ef = evalFunctionList.get(k);
+
+            // average the problem results 5 times
+            // get max value, number of function evaluations, and time taken to run
+            for (int i = 0; i < samplesList.size(); i++) {
+                Integer samples = samplesList.get(i);
+                Integer toKeep = toKeepList.get(i);
+
+                algorithmStats.append(samples);
+                algorithmStats.append(" ");
+                algorithmStats.append(toKeep);
+                algorithmStats.append(" ");
+
+                // average the problem results 5 times
+                // get max value, number of function evaluations, and time taken to run
+                double totalOptimalValues = 0.0;
+                int totalFunctionEvaluations = 0;
+                long totalTimeTakenToRun = 0;
+                int trials = 3;
+                for (int j = 0; j < trials; j++) {
+                    MIMIC mimic = new MIMIC(samples, toKeep, pop);
+                    FixedIterationTrainer fit = new FixedIterationTrainer(mimic, 1000);
+                    fit.train();
+                    System.out.println(ef.value(mimic.getOptimal()));
+
+                    // long startTime = System.currentTimeMillis();
+                    fit.train();
+                    // long endTime = System.currentTimeMillis();
+                    totalOptimalValues += ef.value(mimic.getOptimal());
+                    totalFunctionEvaluations += mimic.getTotalValueEvaluationsToFindOptimalValue();
+                    totalTimeTakenToRun += mimic.getTimeTakenToFindBestValue();
+                }
+                algorithmStats.append(totalOptimalValues / trials);
+                algorithmStats.append(" ");
+                algorithmStats.append(totalFunctionEvaluations / trials);
+                algorithmStats.append(" ");
+                algorithmStats.append(totalTimeTakenToRun / trials);
+                algorithmStats.append("\n");
+                System.out.println("Avg Optimal Value: " + totalOptimalValues / trials);
+                System.out.println("Avg Number of functional evaluations: " + totalFunctionEvaluations / trials);
+                System.out.println("Avg time taken to run: " + totalTimeTakenToRun / trials);
+            }
+            System.out.println(algorithmStats);
+            writeTestStatsToFile(algorithmStats, directoryName, "MIMIC_Stats.txt");
         }
     }
 }
